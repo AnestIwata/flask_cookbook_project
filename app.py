bad9974752ca82eb76516564dbc8b9e9c9f0e1c2
AnastIwata <>
2019-06-24 22:44:40 +0100

    Route in app.py

--- app.py
+++ app.py
@@ -5,5 +5,12 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
+
+# Homepage route
+@app.route('/')
+def home_page():
+    return render_template("index.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
