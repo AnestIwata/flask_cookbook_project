5d47678971cfbbf584dac749eec9acb557f480ff
Anestiwata <pawelpelech@gmail.com>
2019-07-07 12:56:47 +0100

    Created basic forms

---
+++ app/forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtf.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+class RecipeForm(FlaskForm):
+    name = StringField('Recipe Name', validators=[DataRequired()])
+    content = TextAreaField('Your recipe instructions: ', validators=[Length(min=0, max=500)])
+    ingredients = StringField('Ingredients', validators=[DataRequired()])
+    allergens = StringField('Allergens', validators=[DataRequired()])
+    cuisine = SelectField('Cuisine', choices= ['American', 'Mexican'], validators=[DataRequired()])
+    country = SelectField('Cuisine', choices= ['USA', 'Mexico'], validators=[DataRequired()])
+
+    submit = SubmitField('Submit')
