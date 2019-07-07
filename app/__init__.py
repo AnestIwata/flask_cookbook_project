c593edddaf60166309dea7b32bc665b0e486dfe7
Anestiwata <pawelpelech@gmail.com>
2019-07-07 12:42:42 +0100

    Setting up config file

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,6 @@
 from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
-
+from app import app
 
 # Homepage route
 @app.route('/')
@@ -12,8 +8,10 @@ db = SQLAlchemy(app)
 def home_page():
     return render_template("index.html")
 
+# Login page route
 @app.route('/login')
 def login():
     return render_template("login.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
