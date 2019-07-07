1be1dc82c473110144339cfe04b482607a4f9666
Anestiwata <pawelpelech@gmail.com>
2019-07-07 17:55:10 +0100

    Added represntation functions

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from app import db
+from datetime import datetime
+
 
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -6,21 +8,32 @@ class Recipe(db.Model):
     content = db.Column(db.String(500))
     cuisine = db.Column(db.String(64))
     country = db.Column(db.String(64))
-    
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     ingredients = db.Column(db.String(500))
     allergens = db.Column(db.String(500))
+
+    def __repr__(self):
+        return '<Recipe {}>'.format(self.name) 
 
 class Ingredient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
 
+    def __repr__(self):
+        return '<Ingredient {}>'.format(self.name) 
+
 class Allergen(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
 
+    def __repr__(self):
+        return '<Allergen {}>'.format(self.name) 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)   
