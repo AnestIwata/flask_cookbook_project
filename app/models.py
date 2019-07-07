5873035092c8b2163fb3b362e282a2a378290aa0
Anestiwata <pawelpelech@gmail.com>
2019-07-07 19:30:17 +0100

    Included foreign keys

--- app/models.py
+++ app/models.py
@@ -6,34 +6,41 @@ class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
     content = db.Column(db.String(500))
-    cuisine = db.Column(db.String(64))
-    country = db.Column(db.String(64))
+    cuisine = db.Column(db.String(64), index=True)
+    country = db.Column(db.String(64), index=True)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    ingredients = db.Column(db.String(500))
-    allergens = db.Column(db.String(500))
+    ingredients = db.relationship('Ingredient', backref='includes')
+    allergens = db.relationship('Allergen', backref='may contain')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         return '<Recipe {}>'.format(self.name) 
 
+
 class Ingredient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
+    ingredient_recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'))
 
     def __repr__(self):
         return '<Ingredient {}>'.format(self.name) 
 
+
 class Allergen(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
+    alergen_recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'))
 
     def __repr__(self):
         return '<Allergen {}>'.format(self.name) 
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    recipes = db.relationship('Recipe', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)   
