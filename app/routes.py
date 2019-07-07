befd8254399ed4986d252d1d870816a9f17f8b7e
Anestiwata <>
2019-07-07 12:05:16 +0100

    Organizing file structure

---
+++ app/routes.py
@@ -0,0 +1,18 @@
+from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+
+# Homepage route
+@app.route('/')
+def home_page():
+    return render_template("index.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+if __name__ == "__main__":
+    app.run(debug=True)
