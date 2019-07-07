b0854ad62e7a24a709faa61c8e51a93029e90aba
Anestiwata <pawelpelech@gmail.com>
2019-07-07 15:28:50 +0100

    Configuration for Heroku deployment

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from app import app
+from app.forms import LoginForm, RecipeForm
 
 # Homepage route
 @app.route('/')
@@ -11,7 +12,14 @@ def home_page():
 # Login page route
 @app.route('/login')
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+    return render_template("login.html", title='Login', form=form)
+
+@app.route('/add_recipe', methods=["GET", "POST"])
+def add_recipe():
+    form = RecipeForm()
+    return render_template("addRecipe.html", title='Add Recipe', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
