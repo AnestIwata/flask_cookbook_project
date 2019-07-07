cb5e4faffba964be2eb2f3e0500a2c3258455e85
Anestiwata <pawelpelech@gmail.com>
2019-07-07 17:57:09 +0100

    Added migration functionality in init

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 bootstrap = Bootstrap(app)
+
 
 from app import routes, models
