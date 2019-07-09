efebce5cb0becfc945345342db919702e12b89ca
Anestiwata <pawelpelech@gmail.com>
2019-07-09 22:40:14 +0100

    Added category and user session with UserMixin

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import db
 from datetime import datetime
+from flask_login import UserMixin
 
 
 class Recipe(db.Model):
@@ -11,6 +12,7 @@ class Recipe(db.Model):
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     ingredients = db.relationship('Ingredient', backref='includes')
     allergens = db.relationship('Allergen', backref='may contain')
+    category = db.relationship('Category', backref='is in category')
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
@@ -35,12 +37,18 @@ class Allergen(db.Model):
         return '<Allergen {}>'.format(self.name) 
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     recipes = db.relationship('Recipe', backref='author', lazy='dynamic')
+    country = db.Column(db.String(64))
 
     def __repr__(self):
         return '<User {}>'.format(self.username)   
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    category_recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'))
