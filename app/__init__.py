53acc92f3333ca90c9c19d30e6fe78e7197cd0d6
Anestiwata <pawelpelech@gmail.com>
2019-07-09 22:37:59 +0100

    Added login_manager

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 from config import Config
 
 app = Flask(__name__)
@@ -9,6 +10,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 bootstrap = Bootstrap(app)
-
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models
