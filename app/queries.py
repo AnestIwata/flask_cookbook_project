dc3c5211105eec663438bf2d93a796bf13f8394c
Anestiwata <pawelpelech@gmail.com>
2019-07-16 23:25:30 +0100

    Query formatting changed

--- app/queries.py
+++ app/queries.py
@@ -1,6 +1,5 @@
-from app import app, db
+from app import db
 from app.models import Category 
 
 def categories():
-        return Category.query.all()
-        # return db.session.query(Category.name).all()
+        return db.session.query(Category.name).all()
