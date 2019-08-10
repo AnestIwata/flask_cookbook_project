3d106e409c1d398bf064fc1282a4c1ec3b38e96c
Anestiwata <pawelpelech@gmail.com>
2019-08-10 12:21:21 +0100

    Adding classes to shell context for testing

--- cookbook.py
+++ cookbook.py
@@ -1,6 +1,6 @@
 from app import app, db
-from app.models import User, Recipe
+from app.models import User, Recipe, Cuisine, Country
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Recipe': Recipe}
+    return {'db': db, 'User': User, 'Recipe': Recipe, 'Cuisine': Cuisine, 'Country': Country}
