e022f605cb98a5505d8bb0076aa0db23f395c18c
Anestiwata <pawelpelech@gmail.com>
2019-08-10 12:20:42 +0100

    Association table for Ingredients

--- app/models.py
+++ app/models.py
@@ -3,17 +3,41 @@ from datetime import datetime
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
+ingredients = db.Table(
+    'ingredients',
+    db.Column('ingredient_id', db.Integer, db.ForeignKey('ingredient.id')),
+    db.Column('recipe_id', db.Integer, db.ForeignKey('recipe.id'))
+)
 
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    recipes = db.relationship('Recipe', backref='author', lazy='dynamic')
+    country_id = db.Column(db.Integer, db.ForeignKey('country.id'))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username) 
+    
+        
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
     content = db.Column(db.String(500))
-    cuisine = db.Column(db.String(64), index=True)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     ingredients = db.relationship('Ingredient', backref='includes')
     allergens = db.relationship('Allergen', backref='may contain')
-    category = db.relationship('Category', backref='is in category')
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    cuisine_id = db.Column(db.Integer, db.ForeignKey('cuisine.id'))
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
 
     def __repr__(self):
         return '<Recipe {}>'.format(self.name) 
@@ -37,22 +61,6 @@ class Allergen(db.Model):
         return '<Allergen {}>'.format(self.name) 
 
 
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-    recipes = db.relationship('Recipe', backref='author', lazy='dynamic')
-    country = db.Column(db.String(64))
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-    def __repr__(self):
-        return '<User {}>'.format(self.username)   
 
 @login.user_loader
 def load_user(id):
@@ -61,7 +69,7 @@ def load_user(id):
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
-    category_recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'))
+    recipes = db.relationship('Recipe', backref='category', lazy='dynamic')
 
     def __repr__(self):
         return '<Category {}>'.format(self.name) 
@@ -72,6 +80,7 @@ class Category(db.Model):
 class Country(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
+    users = db.relationship('User', backref='country', lazy='dynamic')
 
     def __repr__(self):
         return '<Country {}>'.format(self.name) 
@@ -82,6 +91,7 @@ class Country(db.Model):
 class Cuisine(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
+    recipes = db.relationship('Recipe', backref='cuisine', lazy='dynamic')
 
     def __repr__(self):
         return '<Cuisine {}>'.format(self.name) 
