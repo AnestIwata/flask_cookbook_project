425fd515ec905fd451e0754836b20d9cfa29d3b8
Anestiwata <pawelpelech@gmail.com>
2019-08-11 12:56:57 +0100

    Bat script updated

--- script.bat
+++ script.bat
@@ -1,3 +1,6 @@
+del "C:\Users\Dev\Documents\programowanie\flask_cookbook_project\app.db"
+@RD /S /Q "C:\Users\Dev\Documents\programowanie\flask_cookbook_project\migrations"
+
 flask db init
 flask db migrate
 flask db upgrade
