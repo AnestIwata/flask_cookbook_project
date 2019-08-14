77d56e6b268631313a8866b68d80e324f465d1e8
Anestiwata <pawelpelech@gmail.com>
2019-08-14 20:21:46 +0100

    Pep8 formatting

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'biggest-secret-ever'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_FOLDER = os.path.abspath("app/static/img/recipes_images")
     # LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
--- cookbook.py
+++ cookbook.py
@@ -1,6 +1,7 @@
 from app import app, db
 from app.models import User, Recipe, Cuisine, Country, Ingredient
 
+
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Recipe': Recipe, 'Cuisine': Cuisine, 'Country': Country, 'Ingredient': Ingredient}
