3bd9062f196b48076dd5b8f97917fc8d294feefb
Rafal Rosa <rrosa@poppulo.com>
2019-09-24 23:34:43 +0100

    New chart and removed unused search functionality

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,14 @@
 import os, re, json, sqlalchemy
 from collections import Counter
 from datetime import datetime 
-from flask import Flask, flash, redirect, render_template, request, url_for, jsonify, session, g, current_app
+from flask import Flask, flash, redirect, render_template, request, url_for, jsonify, session
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug import secure_filename
 from werkzeug.urls import url_parse
 from app import app, db
-from app.forms import LoginForm, RecipeForm, ContactForm, RegistrationForm, SearchForm, CommentForm, SearchForm
+from app.forms import LoginForm, RecipeForm, ContactForm, RegistrationForm, SearchForm, CommentForm
 from app.models import Recipe, User, Ingredient, Country, Category, Allergen, Cuisine, Comment
 from app.poppulate_database import Poppulate
-from sqlalchemy import bindparam
-from sqlalchemy.ext import baked
 
 # Homepage route
 @app.route('/')
@@ -82,7 +80,6 @@ def add_recipe():
         form = form_ingredients_and_allergens(RecipeForm(recipe_name=""))
         if form.validate_on_submit():
             f = form.image.data
-            print(form.image.data)
             filename = secure_filename(f.filename)
             file_path = os.path.join(
                 "app/static/img/recipes_images",  filename)
@@ -215,7 +212,6 @@ def recipe(recipe_name):
     userIsAnAuthor = False
     comment_form = CommentForm()
     comments = Comment.query.filter_by(recipe_id=recipe.id).all()
-    print(comments)
     if current_user.is_authenticated and current_user == recipe.author:
         userIsAnAuthor = True
 
@@ -223,7 +219,6 @@ def recipe(recipe_name):
     short_summary = content[0]
 
     if comment_form.validate_on_submit():
-        print(comment_form.data)
         created_comment = Comment(
             name = comment_form.name.data,
             email = comment_form.email.data,
@@ -247,28 +242,20 @@ def recipe(recipe_name):
 @app.route('/upvote', methods=['POST'])
 def upvote():
     if request.method == "POST":
-
         data_received = json.loads(request.data) 
-        print(data_received)
         recipe = Recipe.query.filter_by(name=data_received['recipe_name']).first()
-        print(recipe)
         if recipe:
             recipe.upvotes += 1
             db.session.commit()
-            print(recipe.upvotes)
             return json.dumps({'upvotes' : str(recipe.upvotes)})
         return json.dumps({'status' : 'no recipe found'})
     return redirect(url_for('index'))
-
-def before_request():
-    db.session.commit()
-    g.search_form = SearchForm()
     
 @app.route('/recipes_list', methods=["GET", "POST"])
 def recipes_list():
     form = form_ingredients_and_allergens(SearchForm())
     page = request.args.get('page', 1, type=int)
-    recipes = Recipe.query.paginate(page, 3, False)
+    recipes = Recipe.query.order_by(Recipe.timestamp.desc()).paginate(page, 9, False)
     empty = False
     if not recipes.items:
         empty = True
@@ -287,15 +274,14 @@ def contact():
 
 @app.route('/recipes_stats', methods=["GET"]) 
 def recipes_stats():
-    recipes = Recipe.query.all()
+    recipes = Recipe.query.limit(10).all()
     recipe_ids = [recipe.category_id for recipe in recipes]
-
+    recipe_names = [recipe.name for recipe in recipes]
+    recipes_upvotes = [recipe.upvotes for recipe in recipes]
     data = Category.query.filter(Category.id.in_(recipe_ids)).all()
     counted_ids = dict(Counter(recipe_ids))
     categories = [category.name for category in data ]
-    print(categories)
-    print(counted_ids)
-    return render_template("recipes_stats.html", categories=json.dumps(categories), count=json.dumps(list(counted_ids.values())))
+    return render_template("recipes_stats.html", categories=json.dumps(categories), count=json.dumps(list(counted_ids.values())), recipes=json.dumps(recipe_names), upvotes=json.dumps(recipes_upvotes))
 
 @app.route('/search_handler', methods=["POST", "GET"])
 def search_handler():
@@ -304,7 +290,6 @@ def search_handler():
     ingredients_ids = request.form.getlist('ingredients')
     allergens_ids = request.form.getlist('allergens')
     any_ingredients = request.form.get('any_ingredients')
-    search_text = request.form.get('search_text')
     sortkey = request.form.get('sortkey')
     form = form_ingredients_and_allergens(SearchForm())
     page = request.args.get('page', 1, type=int)
@@ -337,17 +322,15 @@ def search_handler():
             ~Recipe._allergens.any(Allergen.id.in_(allergens_ids)),
             custom_filter_statement(category_id, cuisine_id)
             ).order_by(Recipe.timestamp
-            ).paginate(page, 3, False)
+            ).paginate(page, 9, False)
     else:
         search_result = Recipe.query.filter(
             ~Recipe._allergens.any(Allergen.id.in_(allergens_ids)),
             custom_filter_statement(category_id, cuisine_id)
         ).order_by(order_recipe_by(sortkey)
-        ).paginate(page, 3, False)
+        ).paginate(page, 9, False)
         
-    print(search_result.items)
     reverse = False if sortkey == 'name' else True
-    print("reverse" + str(reverse) + " and " + str(sortkey))
 
     next_url = url_for('recipes_list', page=search_result.next_num) if search_result.has_next else None
     prev_url = url_for('recipes_list', page=search_result.prev_num) if search_result.has_prev else None
