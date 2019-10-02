086f20a78e254ec9c51567ffd43ca64b98313769
Anestiwata <pawelpelech@gmail.com>
2019-10-02 20:49:44 +0100

    Added edit form and sentry support

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,17 @@
+import sentry_sdk
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from config import Config
+from sentry_sdk.integrations.flask import FlaskIntegration
+
+sentry_sdk.init(
+    dsn="https://a2e152adf1ae4896b10ae1558dc58d25@sentry.io/1767305",
+    integrations=[FlaskIntegration()]
+)
 
 app = Flask(__name__, static_folder='', static_url_path='')
 app.config.from_object(Config)
